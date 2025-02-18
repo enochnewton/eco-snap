71667dd625010cd0fc5b3420b3888678706b95da
enochnewton <enochnewton2@gmail.com>
2025-02-18 13:58:46 +0300

    2nd debugging

--- .eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  rules: {
-    "@typescript-eslint/no-unused-vars": "off", // disable unused vars rule
-    "@typescript-eslint/no-explicit-any": "off", // disable any rule
-  },
-};
